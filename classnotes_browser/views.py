8214194a7df598f305a6da49dbb82befa188fc18
Trak Johnson <trakjohnsonweb@gmail.com>
2017-03-04 21:49:54 +0100

    l

--- classnotes_browser/views.py
+++ classnotes_browser/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import os
 
-# Create your views here.
+
+# def index(request):
+#     return render(request, "classnotes_browser/homepage.html")
+
+def homepage(request):
+    subjects_available = [x[0].split(
+        "/"
+    )[-1] for x in os.walk("classnotes_browser/static/classnotes_browser/md_src/")][1:]
+    print(subjects_available)
+    return render(request, "classnotes_browser/homepage.html")
--- ejm_classes/settings.py
+++ ejm_classes/settings.py
@@ -29,6 +29,7 @@ DEBUG = True
 # Application definition
 
 INSTALLED_APPS = [
+    'classnotes_browser',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
