d325f8be2daee76d447ff6dc945e14a326eeb10c
Trak Johnson <trakjohnsonweb@gmail.com>
2017-03-05 18:17:07 +0100

    removed prints

--- classnotes_browser/views.py
+++ classnotes_browser/views.py
@@ -13,8 +13,6 @@ def homepage(request):
 
 
 def render_md(request, cours, name):
-    print([i[0] for i in os.walk(".")])
     with open("classnotes_browser/static/classnotes_browser/md_src/{}/{}.md".format(cours, name)) as f:
         content = markdown.markdown(f.read())
-        print(content)
     return render(request, "classnotes_browser/md_display.html", {"title": name, "md_in": content})
