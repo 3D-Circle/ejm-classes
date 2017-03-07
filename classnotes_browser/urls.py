889dc583e49e021a91c952e4393a72abb02e896d
Trak Johnson <trakjohnsonweb@gmail.com>
2017-03-07 22:43:19 +0100

    established links between pages and setup the cours list pages

--- classnotes_browser/urls.py
+++ classnotes_browser/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.homepage, name='homepage'),
-    url(r'^cours/(.+)/(.+)/$', views.render_md, name="cours")
+    url(r'^cours/(.{1,7})/$', views.cours_dir, name="cours"),
+    url(r'^cours/(.+)/(.+)/$', views.render_md, name="cours-file")
 ]
--- classnotes_browser/views.py
+++ classnotes_browser/views.py
@@ -1,18 +1,42 @@
 from django.shortcuts import render
-import os
 import markdown
+import glob
+
+
+MD_ROOT = "classnotes_browser/static/classnotes_browser/md_src/"
+
+
+def chunker(seq, size):
+    return [seq[pos:pos + size] for pos in range(0, len(seq), size)]
 
 
 def homepage(request):
-    subjects_available = [x[0].split(
-        "/"
-    )[-1] for x in os.walk("classnotes_browser/static/classnotes_browser/md_src/")][1:]
+    subjects_available = [
+        i.split("\\")[-2] for i in glob.glob("{}*/".format(MD_ROOT))
+    ]
+    subjects_available = chunker(subjects_available, 3)
     print(subjects_available)
-    subjects_available = [["SES", "History", "French"], ["Pomme cuite"]]
     return render(request, "classnotes_browser/homepage.html", {"subj_list": subjects_available})
 
-
 def render_md(request, cours, name):
-    with open("classnotes_browser/static/classnotes_browser/md_src/{}/{}.md".format(cours, name)) as f:
+    with open("{}{}/{}.md".format(MD_ROOT, cours, name)) as f:
         content = markdown.markdown(f.read())
     return render(request, "classnotes_browser/md_display.html", {"title": name, "md_in": content})
+
+
+def cours_dir(request, subject):
+    cours_available = chunker(
+        [i.split("\\")[-1].split(".")[0].replace("_", " ") for i in glob.glob("{}/{}/*".format(MD_ROOT, subject))],
+        3
+    )
+    return render(
+        request,
+        "classnotes_browser/cours_list.html",
+        {
+            "title": subject, "cours_list": [[
+                {
+                    "name": name, "link": name.replace(" ", "_")
+                } for name in triplet] for triplet in cours_available
+            ]
+        }
+    )
