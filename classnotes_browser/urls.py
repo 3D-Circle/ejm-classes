87082385b48d584fdbe0b37a2704307edd76df75
Jingjie Yang <j.yang19@ejm.org>
2017-03-10 22:20:12 +0100

    added function to render images

--- classnotes_browser/urls.py
+++ classnotes_browser/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.homepage, name='homepage'),
+    url(r'^cours/(\w+)/src/(.+)/$', views.show_img, name="cours-image"),
     url(r'^cours/(\w+)/$', views.cours_dir, name="subject"),
     url(r'^cours/(\w+)/(.+)/$', views.render_md, name="cours-file")
 ]
